85758fcb6b1c498c2d088ba319071bd8696021a3
Rachel Benua <rachel.benua@gmail.com>
2026-08-08 10:36:42 -0400

    chore: dejar de trackear build/ en git
    Ya estaba en .gitignore pero 74 archivos habian quedado trackeados
    de antes de que existiera esa regla. Cada compilacion local ensuciaba
    git status con decenas de cambios que no son codigo fuente.

--- build/CMakeFiles/RelojArtistico.dir/src/segment_mondrian.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/RelojArtistico.dir/src/segment_mondrian.c.obj: \
- C:\Users\rache\proyectos\RelojArtistico\src\segment_mondrian.c
--- build/CMakeFiles/RelojArtistico.dir/src/segment_monet.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/RelojArtistico.dir/src/segment_monet.c.obj: \
- C:\Users\rache\proyectos\RelojArtistico\src\segment_monet.c
